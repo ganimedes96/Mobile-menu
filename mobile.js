dad6f716afef990e6dd28c67cc33c6a75d60dc07
hudson <ganimedes409@gmail.com>
2022-03-01 21:25:07 -0300

    Menu mobile

---
+++ mobile.js
@@ -0,0 +1,12 @@
+
+const btnMobile = document.getElementById('btn-mobile')
+
+
+const toggleMenu = () =>{
+    
+    const nav = document.getElementById('nav')
+        nav.classList.toggle('active')    
+
+    }
+
+btnMobile.addEventListener('click', toggleMenu)
